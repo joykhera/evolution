fdb13caf6ebdbeea50225c647eae09d7d8a49da1
joykhera <joykhera@gmail.com>
2023-11-21 14:42:45 -0500

    add testing and killing

--- gym_env/evolution_env.py
+++ gym_env/evolution_env.py
@@ -44,6 +44,18 @@ class EvolutionEnv(MultiAgentEnv):
         human_player=False,
         map_size=SIZE,
         grid_size=GRID_SIZE,
+        map_color=WHITE,
+        food_color=GREEN,
+        player_color=RED,
+        out_of_bounds_color=BLACK,
+        food_count=FOOD_COUNT,
+        fps=FPS,
+        food_size=FOOD_SIZE,
+        player_size=PLAYER_SIZE,
+        player_size_increase=PLAYER_SIZE_INCREASE,
+        player_speed=PLAYER_SPEED,
+        episode_steps=EPISODE_STEPS,
+        scale=SCALE,
     ):
         super().__init__()
         self.num_agents = num_agents
@@ -51,14 +63,26 @@ class EvolutionEnv(MultiAgentEnv):
         self.human_player = human_player
         self.map_size = map_size
         self.grid_size = grid_size
+        self.map_color = map_color
+        self.food_color = food_color
+        self.player_color = player_color
+        self.out_of_bounds_color = out_of_bounds_color
+        self.food_count = food_count
+        self.fps = fps
+        self.food_size = food_size
+        self.player_size = player_size
+        self.player_size_increase = player_size_increase
+        self.player_speed = player_speed
+        self.episode_steps = episode_steps
+        self.scale = scale
 
         if self.render_mode == "human":
             pygame.init()
             pygame.display.init()
-            self.screen = pygame.display.set_mode((map_size * SCALE, map_size * SCALE))
+            self.screen = pygame.display.set_mode((map_size * scale, map_size * scale))
             pygame.display.set_caption("Evolution Environment")
             self.clock = pygame.time.Clock()
-            self.window = pygame.display.set_mode((map_size * SCALE, map_size * SCALE))
+            self.window = pygame.display.set_mode((map_size * scale, map_size * scale))
             pygame.font.init()
             self.font = pygame.font.SysFont(None, 24)
         self.canvas = pygame.Surface((map_size, map_size))
@@ -77,6 +101,8 @@ class EvolutionEnv(MultiAgentEnv):
         self.action_space = spaces.Discrete(5)
         self._agent_ids = set(range(self.num_agents))
         self.food_eaten = 0
+        self.total_reward = 0
+        self.kill_reward = 10
 
     def step(self, action_dict):
         """Executes a step for each agent in the environment."""
@@ -98,42 +124,39 @@ class EvolutionEnv(MultiAgentEnv):
 
         dones["__all__"] = all(value for value in dones.values())
         self.steps += 1
+        self.total_reward += sum(rewards.values())
         cur_time = time.perf_counter()
         # print("Step:", self.steps, " Step time: ", cur_time - self.prev_step_time)
         self.prev_step_time = cur_time
-        # self.render(scale=SCALE)
         return observations, rewards, dones, dones, infos
 
     def reset(self, seed=None, options=None):
-        """Resets the game to an initial state."""
+        """Resets the game to an initial state using vectorized operations."""
+        # Generate random positions for the food
+        food_positions = np.random.randint(0, self.map_size, size=(self.food_count, 2))
         self.foods = [
-            Food(
-                position=(
-                    random.randint(0, self.map_size),
-                    random.randint(0, self.map_size),
-                ),
-                size=FOOD_SIZE,
-                color=GREEN,
-            )
-            for _ in range(FOOD_COUNT)
+            Food(position=tuple(pos), size=self.food_size, color=self.food_color)
+            for pos in food_positions
         ]
 
+        # Generate random positions for the players
+        player_positions = np.random.randint(
+            0, self.map_size, size=(self.num_agents, 2)
+        )
         self.players = [
             Player(
-                position=(
-                    random.randint(0, self.map_size),
-                    random.randint(0, self.map_size),
-                ),
+                position=tuple(pos),
                 map_size=self.map_size,
-                size=PLAYER_SIZE,
-                speed=PLAYER_SPEED,
-                color=RED,
+                size=self.player_size,
+                speed=self.player_speed,
+                color=self.player_color,
             )
-            for _ in range(self.num_agents)
+            for pos in player_positions
         ]
-        # self.players[0].color = BLUE
+
         self.steps = 0
         self.food_eaten = 0
+        self.total_reward = 0
         return self._get_obs(), {}
 
     def _get_obs(self):
@@ -163,7 +186,7 @@ class EvolutionEnv(MultiAgentEnv):
         ).astype(int)
 
         # Extract the rectangle of the screen
-        self.render(scale=SCALE)
+        self.render(scale=self.scale)
         screen_pixels = pygame.surfarray.array3d(self.canvas)
         screen_pixels = np.transpose(screen_pixels, axes=(1, 0, 2))
 
@@ -196,40 +219,26 @@ class EvolutionEnv(MultiAgentEnv):
         return observation / 255.0
 
     def _get_reward(self, player_idx):
-        """Returns the reward after an action.
+        """Returns the reward after an action."""
+        reward = 0
 
-        The player gets -0.1 reward for not moving.
-        The player gets a larger penalty for touching a wall.
-        """
-        # Check if the player has eaten food
-        food_reward = int(self._check_collisions(player_idx))
+        # Reward for eating food
+        reward += int(self._check_collisions_with_food(player_idx))
+
+        # Check for collisions with other agents
+        collision_reward = self._check_collisions_with_agents(player_idx)
+        reward += collision_reward
 
         # Check if the player is touching a wall
-        player_position = self.players[player_idx].position
-        wall_penalty = (
-            -1
-            if player_position.x <= 0
-            or player_position.x >= self.map_size
-            or player_position.y <= 0
-            or player_position.y >= self.map_size
-            else 0
-        )
-
-        # Check if the player did not move (assuming you have a way to check the previous position)
-        move_penalty = 0
-        # if (
-        #     self.players[player_idx].position
-        #     == self.players[player_idx].previous_position
-        # ):
-        #     move_penalty = -0.1
+        wall_penalty = self._check_wall_collision(player_idx)
+        reward += wall_penalty
 
         # Combine the rewards
-        reward = food_reward + wall_penalty + move_penalty
         return reward
 
     def render(self, scale=1):
         # Clear the canvas with a black color
-        self.canvas.fill(WHITE)
+        self.canvas.fill(self.map_color)
         for food in self.foods:
             food.draw(self.canvas, 1)
 
@@ -247,8 +256,16 @@ class EvolutionEnv(MultiAgentEnv):
                 scaled_canvas.get_rect(),
             )
             self.window.blit(scaled_canvas, scaled_canvas.get_rect())
-            text_surface = self.font.render(f'Food eaten: {self.food_eaten}', True, BLACK)
-            self.window.blit(text_surface, (10, 10))
+
+            food_eaten_text_surface = self.font.render(
+                f"Food eaten: {self.food_eaten}", True, self.out_of_bounds_color
+            )
+            self.window.blit(food_eaten_text_surface, (10, 10))
+            total_reward_text_surface = self.font.render(
+                f"Total reward: {self.total_reward}", True, self.out_of_bounds_color
+            )
+            self.window.blit(total_reward_text_surface, (10, 25))
+
             pygame.event.pump()
             self.clock.tick(FPS)
             pygame.display.flip()
@@ -289,7 +306,72 @@ class EvolutionEnv(MultiAgentEnv):
             else direction_vector
         )
 
-    def _check_collisions(self, player_idx):
+    def _check_wall_collision(self, player_idx):
+        """Check if the player is touching a wall."""
+        player_position = self.players[player_idx].position
+        if (
+            player_position.x <= 0
+            or player_position.x >= self.map_size
+            or player_position.y <= 0
+            or player_position.y >= self.map_size
+        ):
+            return -1  # Penalty for touching a wall
+        return 0
+
+    def _check_collisions_with_agents(self, player_idx):
+        """Check for collisions with other agents and adjust rewards using NumPy."""
+        player_positions = np.array([p.position for p in self.players])
+        player_sizes = np.array([int(p.size) for p in self.players])
+
+        current_position = player_positions[player_idx]
+        current_size = int(player_sizes[player_idx])
+
+        # Calculate distances from the current player to all other players
+        distances = np.linalg.norm(player_positions - current_position, axis=1)
+
+        # Calculate the sum of radii (considering each player's size)
+        radii_sum = (current_size + player_sizes) / 2
+
+        # Check for collisions (distance smaller than radii sum)
+        collisions = distances < radii_sum
+
+        # Exclude the current player from collision check
+        collisions[player_idx] = False
+
+        # Assign rewards or penalties for collisions
+        rewards = np.zeros(player_sizes.shape)
+        bigger = player_sizes > current_size
+        smaller = player_sizes < current_size
+        rewards[bigger & collisions] = self.kill_reward
+        rewards[smaller & collisions] = -self.kill_reward
+
+        # Sum up the rewards from collisions
+        total_reward = rewards.sum()
+
+        return total_reward
+
+    # def _check_collisions_with_food(self, player_idx):
+    #     """Vectorized check for collisions between player and food."""
+    #     # Convert food positions and player position to NumPy arrays for vectorized operations
+    #     food_positions = np.array([food.position for food in self.foods])
+    #     player_position = np.array(self.players[player_idx].position)
+
+    #     # Calculate distances in a vectorized way
+    #     distances = np.linalg.norm(food_positions - player_position, axis=1)
+
+    #     # Find collided food items
+    #     collisions = distances < self.players[player_idx].size
+
+    #     # Update food positions and player size for each collision
+    #     for food_idx, collided in enumerate(collisions):
+    #         if collided:
+    #             # self.foods[food_idx].set_position(...)  # Set new food position
+    #             self.players[player_idx].size += self.player_size_increase
+    #             self.food_eaten += 1
+
+    #     return np.any(collisions)
+
+    def _check_collisions_with_food(self, player_idx):
         """Checks for collisions between player and food."""
         for food in self.foods[:]:
             if (
@@ -297,11 +379,10 @@ class EvolutionEnv(MultiAgentEnv):
                 < self.players[player_idx].size
             ):
                 # self.foods.remove(food)
-                # print("Food eaten by player ", player_idx)
                 food.set_position(
                     (random.randint(0, self.map_size), random.randint(0, self.map_size))
                 )
-                self.players[player_idx].size += PLAYER_SIZE_INCREASE
+                self.players[player_idx].size += self.player_size_increase
                 self.food_eaten += 1
                 return True
 
@@ -309,8 +390,8 @@ class EvolutionEnv(MultiAgentEnv):
 
     def _is_done(self):
         """Checks if the game is finished."""
-        return self.steps >= EPISODE_STEPS
-        # return self.steps >= EPISODE_STEPS or self.foods == []
+        return self.steps >= self.episode_steps
+        # return self.steps >= self.episode_steps or self.foods == []
 
     def test_ai(self, nets, num_steps=1000):
         """Test the AI's performance in the game for a certain number of steps."""
@@ -329,8 +410,8 @@ class EvolutionEnv(MultiAgentEnv):
             ]
 
             self.step(actions)
-            self.render(scale=SCALE)
-            self.clock.tick(FPS)
+            self.render(scale=self.scale)
+            self.clock.tick(self.fps)
 
             if self._is_done():
                 break
--- main.py
+++ main.py
@@ -8,17 +8,32 @@ from gym_env.evolution_env import EvolutionEnv
 from gymnasium import spaces
 import numpy as np
 import os
+import argparse
 
 log_dir = os.path.join(os.getcwd(), "training")
 
 # check_env(EvolutionEnv())
 
+map_size = 100
+
 env_config = {
     "num_agents": 10,
-    # "render_mode": None,
-    "render_mode": 'human',
-    "map_size": 100,
+    "render_mode": None,
+    # "render_mode": 'human',
+    "map_size": map_size,
     "grid_size": 42,
+    "map_color": (255, 255, 255),
+    "food_color": (0, 255, 0),
+    "player_color": (255, 0, 0),
+    "out_of_bounds_color": (0, 0, 0),
+    "food_count": 50,
+    "fps": 120,
+    "food_size": map_size / 50,
+    "player_size": map_size / 50,
+    "player_size_increase": 0.25,
+    "player_speed": map_size / 100,
+    "episode_steps": 200,
+    "scale": 5,
 }
 
 episode_length = 200
@@ -31,11 +46,19 @@ def env_creator(env_config):
         render_mode=env_config["render_mode"],
         map_size=env_config["map_size"],
         grid_size=env_config["grid_size"],
+        map_color=env_config["map_color"],
+        food_color=env_config["food_color"],
+        player_color=env_config["player_color"],
+        out_of_bounds_color=env_config["out_of_bounds_color"],
+        food_count=env_config["food_count"],
+        fps=env_config["fps"],
+        food_size=env_config["food_size"],
+        player_size=env_config["player_size"],
+        player_size_increase=env_config["player_size_increase"],
+        player_speed=env_config["player_speed"],
+        episode_steps=env_config["episode_steps"],
+        scale=env_config["scale"],
     )
-
-
-def policy_mapping_fn(agent_id, **kwargs):
-    return "default_policy"
 
 
 register_env("EvolutionEnv", env_creator)
@@ -46,7 +69,7 @@ ray.init()
 # Configure the environment and the PPO agent
 config = {
     "env": "EvolutionEnv",
-    "num_workers": 0,
+    "num_workers": 9,
     "framework": "tf",
     "timesteps_per_iteration": episode_length,
     # "min_sample_timesteps_per_reporting": episode_length,
@@ -72,17 +95,60 @@ config = {
     "env_config": env_config,
 }
 
-env = EvolutionEnv()
-# Run the training algorithm
-tune.run(
-    PPO,
-    config=config,
-    stop={"training_iteration": 50},
-    local_dir=log_dir,
-    checkpoint_freq=10,
-    checkpoint_at_end=True,
-    reuse_actors=True,
-)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-test", action="store_true", help="Enable testing mode.")
+    parser.add_argument(
+        "-checkpoint", type=str, help="Checkpoint from which to load the model."
+    )
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    if args.test and args.checkpoint:
+        # Set up the environment for testing
+        env_config["render_mode"] = "human"
+        env_config["episode_steps"] = 1000
+        config["num_workers"] = 0
+        env = env_creator(env_config)
+
+        # Create a PPO trainer instance
+        trainer = PPO(env="EvolutionEnv", config=config)
+
+        # Restore from the checkpoint
+        trainer.restore(args.checkpoint)
+
+        # Run the testing loop
+        for _ in range(10):
+            episode_reward = 0
+            done = False
+            obs, _ = env.reset()  # This should return a dictionary of observations
+
+            while not done:
+                action_dict = {}
+                for agent_id, agent_obs in obs.items():
+                    # For each agent, compute an action using its specific observation
+                    action_dict[agent_id] = trainer.compute_single_action(
+                        agent_obs, policy_id="policy_0"
+                    )
+                # Step the environment with the actions of all agents
+                obs, rewards, dones, _, infos = env.step(action_dict)
+
+                # Accumulate rewards and check if all agents are done
+                episode_reward += sum(rewards.values())
+                done = all(dones.values())
+
+            print(f"Episode reward: {episode_reward}")
+    else:
+        tune.run(
+            PPO,
+            config=config,
+            stop={"training_iteration": 100},
+            local_dir=log_dir,
+            checkpoint_freq=10,
+            checkpoint_at_end=True,
+            reuse_actors=True,
+        )
 
 # Shutdown Ray
 ray.shutdown()
